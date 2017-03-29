329a0867b2ad1f21cab794edea7553e0387a583c
Ricardo García Fernández <ricardogarfe@gmail.com>
2017-03-29 17:02:33 +0200

    #1 - Create first unit test for fizzbuzz

---
+++ fizzbuzz/src/test/java/com/koukio/internals/FizzBuzzTest.java
@@ -0,0 +1,19 @@
+package com.koukio.internals;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Unit test for FizzBuzz.
+ */
+public class FizzBuzzTest {
+
+    /**
+     * Test Fizz Buzz For Fizz
+     */
+    @org.junit.Test
+    public void testFizzBuzzForFizz() throws Exception {
+        assertThat(true, is(true));
+    }
+
+}
