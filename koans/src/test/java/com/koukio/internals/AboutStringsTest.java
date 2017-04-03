37ac7590a9a9c728ae293502f828c24bfba49c07
joan fornes <joan.fornes@koukio.com>
2017-04-03 10:59:22 +0200

    AboutStringTest ok

--- koans/src/test/java/com/koukio/internals/AboutStringsTest.java
+++ koans/src/test/java/com/koukio/internals/AboutStringsTest.java
@@ -27,40 +27,40 @@ public class AboutStringsTest {
     public void newStringIsRedundant() {
         String stringInstance = "zero";
         String stringReference = new String(stringInstance);
-        Assert.assertEquals(stringInstance.equals(stringReference), "__");
+        Assert.assertEquals(stringInstance.equals(stringReference), true);
     }
 
     @Test
     public void newStringIsNotIdentical() {
         String stringInstance = "zero";
         String stringReference = new String(stringInstance);
-        Assert.assertEquals(stringInstance == stringReference, "__");
+        Assert.assertEquals(stringInstance == stringReference, false);
     }
 
     @Test
     public void stringIsEmpty() {
-        Assert.assertEquals("".isEmpty(), false);
+        Assert.assertEquals("".isEmpty(), true);
         Assert.assertEquals("one".isEmpty(), false);
-        Assert.assertEquals(new String().isEmpty(), false);
-        Assert.assertEquals(new String("").isEmpty(), false);
+        Assert.assertEquals(new String().isEmpty(), true);
+        Assert.assertEquals(new String("").isEmpty(), true);
         Assert.assertEquals(new String("one").isEmpty(), false);
     }
 
     @Test
     public void stringLength() {
-        Assert.assertEquals("".length(), "__");
-        Assert.assertEquals("one".length(), "__");
-        Assert.assertEquals("the number is one".length(), "__");
+        Assert.assertEquals("".length(), 0);
+        Assert.assertEquals("one".length(), 3);
+        Assert.assertEquals("the number is one".length(), 17);
     }
 
     @Test
     public void stringTrim() {
-        Assert.assertEquals("".trim(), "__");
+        Assert.assertEquals("".trim(), "");
         Assert.assertEquals("one".trim(), "one");
-        Assert.assertEquals(" one more time".trim(), "__");
-        Assert.assertEquals(" one more time         ".trim(), "__");
-        Assert.assertEquals(" and again\t".trim(), "__");
-        Assert.assertEquals("\t\t\twhat about now?\t".trim(), "__");
+        Assert.assertEquals(" one more time".trim(), "one more time");
+        Assert.assertEquals(" one more time         ".trim(), "one more time");
+        Assert.assertEquals(" and again\t".trim(), "and again");
+        Assert.assertEquals("\t\t\twhat about now?\t".trim(), "what about now?");
     }
 
     @Test
@@ -68,107 +68,107 @@ public class AboutStringsTest {
         String one = "one";
         String space = " ";
         String two = "two";
-        Assert.assertEquals(one + space + two, "__");
-        Assert.assertEquals(space + one + two, "__");
-        Assert.assertEquals(two + space + one, "__");
+        Assert.assertEquals(one + space + two, "one two");
+        Assert.assertEquals(space + one + two, " onetwo");
+        Assert.assertEquals(two + space + one, "two one");
     }
 
     @Test
     public void stringUpperCase() {
         String str = "I am a number one!";
-        Assert.assertEquals(str.toUpperCase(), "__");
+        Assert.assertEquals(str.toUpperCase(), "I AM A NUMBER ONE!");
     }
 
     @Test
     public void stringLowerCase() {
         String str = "I AM a number ONE!";
-        Assert.assertEquals(str.toLowerCase(), "__");
+        Assert.assertEquals(str.toLowerCase(), "i am a number one!");
     }
 
     @Test
     public void stringCompare() {
         String str = "I AM a number ONE!";
-        Assert.assertEquals(str.compareTo("I AM a number ONE!") == 0, "__");
-        Assert.assertEquals(str.compareTo("I am a number one!") == 0, "__");
-        Assert.assertEquals(str.compareTo("I AM A NUMBER ONE!") == 0, "__");
+        Assert.assertEquals(str.compareTo("I AM a number ONE!") == 0, true);
+        Assert.assertEquals(str.compareTo("I am a number one!") == 0, false);
+        Assert.assertEquals(str.compareTo("I AM A NUMBER ONE!") == 0, false);
     }
 
     @Test
     public void stringCompareIgnoreCase() {
         String str = "I AM a number ONE!";
-        Assert.assertEquals(str.compareToIgnoreCase("I AM a number ONE!") == 0, "__");
-        Assert.assertEquals(str.compareToIgnoreCase("I am a number one!") == 0, "__");
-        Assert.assertEquals(str.compareToIgnoreCase("I AM A NUMBER ONE!") == 0, "__");
+        Assert.assertEquals(str.compareToIgnoreCase("I AM a number ONE!") == 0, true);
+        Assert.assertEquals(str.compareToIgnoreCase("I am a number one!") == 0, true);
+        Assert.assertEquals(str.compareToIgnoreCase("I AM A NUMBER ONE!") == 0, true);
     }
 
     @Test
     public void stringStartsWith() {
-        Assert.assertEquals("".startsWith("one"), "__");
-        Assert.assertEquals("one".startsWith("one"), "__");
-        Assert.assertEquals("one is the number".startsWith("one"), "__");
-        Assert.assertEquals("ONE is the number".startsWith("one"), "__");
+        Assert.assertEquals("".startsWith("one"), false);
+        Assert.assertEquals("one".startsWith("one"), true);
+        Assert.assertEquals("one is the number".startsWith("one"), true);
+        Assert.assertEquals("ONE is the number".startsWith("one"), false);
     }
 
     @Test
     public void stringEndsWith() {
-        Assert.assertEquals("".endsWith("one"), "__");
-        Assert.assertEquals("one".endsWith("one"), "__");
-        Assert.assertEquals("the number is one".endsWith("one"), "__");
-        Assert.assertEquals("the number is two".endsWith("one"), "__");
-        Assert.assertEquals("the number is One".endsWith("one"), "__");
+        Assert.assertEquals("".endsWith("one"), false);
+        Assert.assertEquals("one".endsWith("one"), true);
+        Assert.assertEquals("the number is one".endsWith("one"), true);
+        Assert.assertEquals("the number is two".endsWith("one"), false);
+        Assert.assertEquals("the number is One".endsWith("one"), false);
     }
 
     @Test
     public void stringSubstring() {
         String str = "I AM a number ONE!";
-        Assert.assertEquals(str.substring(0), "__");
-        Assert.assertEquals(str.substring(1), "__");
-        Assert.assertEquals(str.substring(5), "__");
-        Assert.assertEquals(str.substring(14, 17), "__");
-        Assert.assertEquals(str.substring(7, str.length()), "__");
+        Assert.assertEquals(str.substring(0),"I AM a number ONE!");
+        Assert.assertEquals(str.substring(1), " AM a number ONE!");
+        Assert.assertEquals(str.substring(5), "a number ONE!");
+        Assert.assertEquals(str.substring(14, 17), "ONE");
+        Assert.assertEquals(str.substring(7, str.length()), "number ONE!");
     }
 
     @Test
     public void stringContains() {
         String str = "I AM a number ONE!";
-        Assert.assertEquals(str.contains("one"), "__");
-        Assert.assertEquals(str.contains("ONE"), "__");
+        Assert.assertEquals(str.contains("one"), false);
+        Assert.assertEquals(str.contains("ONE"), true);
     }
 
     @Test
     public void stringReplace() {
         String str = "I am a number ONE!";
-        Assert.assertEquals(str.replace("ONE", "TWO"), "__");
-        Assert.assertEquals(str.replace("I am", "She is"),  "__");
+        Assert.assertEquals(str.replace("ONE", "TWO"), "I am a number two!");
+        Assert.assertEquals(str.replace("I am", "She is"), "She is a number ONE!");
     }
 
     @Test
     public void stringBuilderCanActAsAMutableString() {
-        Assert.assertEquals(new StringBuilder("one").append(" ").append("two").toString(), "__");
+        Assert.assertEquals(new StringBuilder("one").append(" ").append("two").toString(), "one two");
     }
 
     @Test
     public void readableStringFormattingWithStringFormat() {
-        Assert.assertEquals(String.format("%s %s %s", "a", "b", "a"), "__");
+        Assert.assertEquals(String.format("%s %s %s", "a", "b", "a"), "a b a");
     }
 
     @Test
     public void extraArgumentsToStringFormatGetIgnored() {
-        Assert.assertEquals(String.format("%s %s %s", "a", "b", "c", "d"), "__");
+        Assert.assertEquals(String.format("%s %s %s", "a", "b", "c", "d"), "a b c");
     }
 
     @Test
     public void readableStringFormattingWithMessageFormat() {
-        Assert.assertEquals(MessageFormat.format("{0} {1} {0}", "a", "b"), "__");
+        Assert.assertEquals(MessageFormat.format("{0} {1} {0}", "a", "b"), "a b a");
     }
 
     @Test
     public void extraArgumentsToMessageFormatGetIgnored() {
-        Assert.assertEquals(MessageFormat.format("{0} {1} {0}", "a", "b", "c"), "__");
+        Assert.assertEquals(MessageFormat.format("{0} {1} {0}", "a", "b", "c"), "a b a");
     }
 
     @Test
     public void insufficientArgumentsToMessageFormatDoesNotReplaceTheToken() {
-        Assert.assertEquals(MessageFormat.format("{0} {1} {0}", "a"), "__");
+        Assert.assertEquals(MessageFormat.format("{0} {1} {0}", "a"), "a {1} a");
     }
 }
